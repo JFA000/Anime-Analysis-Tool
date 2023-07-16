829c4ebb4b4282e6d8cd4cfb7437198123da81f0
JFA000 <jonatasfernandesandrade@gmail.com>
2023-07-16 16:12:15 -0300

    Update `prepare_dataset.py`

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,16 +2,15 @@ import nltk, pandas as pd
 from nltk.corpus import stopwords
 
 nltk.download('stopwords')
-df = pd.read_csv('old_anime_with_synopsis.csv')
+df = pd.read_csv('anime_with_synopsis.csv')
 
 def remove_stopwords(df):
     stop_words = set(stopwords.words('english'))
     df['sypnopsis'] = df['sypnopsis'].fillna('')
     df['sypnopsis'] = df['sypnopsis'].apply(lambda x: ' '.join([word for word in x.split() if word.lower() not in stop_words]))
     df = df.rename(columns={'sypnopsis': 'synopsis'})
-    #df = df.drop(columns=['MAL_ID', 'Score', 'Genres']) Not needed.
     return df
 
 df = remove_stopwords(df)
-df.to_csv('anime_with_synopsis.csv', index=False)
+df.to_csv('anime_with_synopsis_cleaned.csv', index=False)
 print("Execution successful. The database 'anime_with_synopsis.csv' is now available for use.")
