a2e6fd375f483f22762e97fe435f25850e28dc2a
JFA000 <jonatasfernandesandrade@gmail.com>
2023-07-22 19:09:51 -0300

    :recycle: refactor: Add functions for code readability

--- anime_analysis.py
+++ anime_analysis.py
@@ -1,5 +1,5 @@
 #region Imports
-import pandas as pd, matplotlib.pyplot as plt
+import pandas as pd, matplotlib.pyplot as plt, csv
 #endregion
 #region Variables definition
 csv_path = './anime_with_synopsis.csv' # Set the path of the CSV file
@@ -21,6 +21,7 @@ filtered_df = df[df['Score'] != 'Unknown'] #Remove Unknown Values
 filtered_df.loc[:, 'Score'] = pd.to_numeric(filtered_df['Score'], errors='coerce')
 filtered_df = filtered_df.dropna(subset=['Score'])
 sorted_df = filtered_df.sort_values('Score', ascending=False) #Sort in descending order
+sorted_df['Score'] = sorted_df['Score'].astype(float)
 top_10_animes = sorted_df.head(10)
 print(top_10_animes[['Name', 'Score']].to_string(index=False))
 
@@ -43,86 +44,67 @@ def filter_by_genre(genre):
     top_10_animes = sorted_df.head(10)
     return top_10_animes[['Name', 'Score']]
 
-print("\nGenre list")
-genre_dict = {
-    1: 'Action',
-    2: 'Adventure',
-    3: 'Cars',
-    4: 'Comedy',
-    5: 'Dementia',
-    6: 'Demons',
-    7: 'Drama',
-    8: 'Ecchi',
-    9: 'Fantasy',
-    10: 'Game',
-    11: 'Genres',
-    12: 'Harem',
-    13: 'Historical',
-    14: 'Horror',
-    15: 'Josei',
-    16: 'Kids',
-    17: 'Magic',
-    18: 'Martial Arts',
-    19: 'Mecha',
-    20: 'Military',
-    21: 'Music',
-    22: 'Mystery',
-    23: 'Parody',
-    24: 'Police',
-    25: 'Psychological',
-    26: 'Romance',
-    27: 'Samurai',
-    28: 'School',
-    29: 'Sci-Fi',
-    30: 'Seinen',
-    31: 'Shoujo',
-    32: 'Shoujo Ai',
-    33: 'Shounen',
-    34: 'Shounen Ai',
-    35: 'Slice of Life',
-    36: 'Space', 
-    37:'Sports', 
-    38:'Super Power', 
-    39:'Supernatural', 
-    40:'Thriller', 
-    41:'Unknown', 
-    42:'Vampire', 
-    43:'Yaoi'
-}
+def get_genres(csv_path):
+    genres = set()
+    genre_dict = {}
+    with open(csv_path,encoding='utf-8' ,newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            genres.update(row[3].split(','))
 
-for key, value in genre_dict.items():
-   print(f"{key}: {value}")
-genre = genre_dict.get(int(input("Enter the genre number you want to select (1-43): ")))
-print(f"You selected {genre}.\nShowing top 10 animes with the {genre} genre: ")
+    genres = [genre.strip() for genre in genres]#Remove spaces
+    genres = set(genres)#Remove duplicates
+    genres = sorted(genres)#Sort in order
 
-top10chosen = filter_by_genre(genre)
+    for idx, genre in enumerate(genres, 1):
+        genre_dict[idx] = genre
+    
+    return genre_dict
+
+def plot(df,dfxlabel,dfylabel,dftitle):
+    
+    x = df[dfxlabel].values[:10]
+    y = df[dfylabel].values[:10]
+    
+
+    plt.figure(figsize=(13, 7)) # 1300*700
+    plt.xticks(rotation=90)
+    plt.xlabel(dfxlabel)
+    plt.ylabel(dfylabel)
+    plt.title(dftitle)
+    plt.ylim(min(y)-0.1, max(y)+0.01) 
+    
+    plt.bar(x, y)
+    plt.show()
+
+genre_dict = get_genres(csv_path)
+def select_genre():    
+    for key, value in genre_dict.items():
+       print(f"{key}: {value}")
+    genre = genre_dict.get(int(input("Enter the genre number you want to select (1-43): ")))
+    print(f"You selected {genre}.\nShowing top 10 animes with the {genre} genre: ")
+    return genre
+
+top10chosen = filter_by_genre(select_genre())
 print(top10chosen.to_string(index=False))
-#Using Matplotlib
 
-sorted_df['Score'] = sorted_df['Score'].astype(float)
-x = sorted_df['Name'].values[:10]
-y = sorted_df['Score'].values[:10]
+plot(sorted_df,'Name','Score','Top 10 Animes by Score')
 
-plt.figure(figsize=(10, 6)) # ajustando o tamanho da figura
 
-plt.bar(x, y)
+result = {}
+for genre in genre_dict.values():
+    if genre != "Genres":
+        mask = sorted_df['Genres'].str.contains(genre)
+        genre_df = sorted_df[mask]
+        result[genre] = genre_df['Score'].mean()
 
-# adicionando os nomes dos animes no eixo x
-plt.xticks(x, x)
+genres_average_df = pd.DataFrame(result.items(), columns=['Genre', 'Average Score'])
+print(genres_average_df)
 
-# rotacionando os rótulos do eixo x para melhor legibilidade
-plt.xticks(rotation=90)
-
-# nomeando o eixo x
-plt.xlabel('Anime')
-
-# nomeando o eixo y
-plt.ylabel('Score')
-
-# dando um título ao gráfico
-plt.title('Top 10 Anime por Score')
-
-plt.ylim(min(y) - 0.5, max(y) + 0.5) 
-
-# mostrando o gráfico
+plt.figure(figsize=(10, 6))
+plt.bar(genres_average_df['Genre'], genres_average_df['Average Score'])
+plt.xlabel('Genre')
+plt.ylabel('Average Score')
+plt.title('Genre average score')
+plt.ylim(min(genres_average_df['Average Score'])-0.1, max(genres_average_df['Average Score'])+0.01) 
 plt.show()
