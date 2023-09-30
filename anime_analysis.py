e20f62a3955d7ee04a81d0756d55577597826001
JFA000 <jonatasfernandesandrade@gmail.com>
2023-09-30 14:57:01 -0300

    Changed genre selection to be misstype proof

--- anime_analysis.py
+++ anime_analysis.py
@@ -1,4 +1,6 @@
+#region Program Startup
 import pandas as pd, matplotlib.pyplot as plt, seaborn as sns ,csv
+from matplotlib.ticker import FixedLocator
 csv_path = './anime_with_synopsis.csv' # Set the path of the CSV file
 chunk_size = 4000  # Set the chunk size
 df = pd.DataFrame() # Instantiate an empty DataFrame
@@ -6,29 +8,9 @@ selected_title = None
 for chunk in pd.read_csv(csv_path, chunksize=chunk_size):
     df = pd.concat([df, chunk], axis=0)
 
-print("\n╔══════════════════════════════════════════╗")
-print("║  Welcome to the anime analysis program!  ║")
-print("╚══════════════════════════════════════════╝")
 
-print("\nTop 10 animes ranked by score:\n")
-filtered_df = df[df['Score'] != 'Unknown'] #Remove Unknown Values
-filtered_df.loc[:, 'Score'] = pd.to_numeric(filtered_df['Score'], errors='coerce')
-filtered_df = filtered_df.dropna(subset=['Score'])
-sorted_df = filtered_df.sort_values('Score', ascending=False) #Sort in descending order
-sorted_df['Score'] = sorted_df['Score'].astype(float)
-top_10_animes = sorted_df.head(10)
-print(top_10_animes[['Name', 'Score']].to_string(index=False))
-
-print("\nThe 10 genres that appear the most among them are:")
-genre_lists = top_10_animes['Genres'].str.split(',')
-genres = [genre.strip() for genre_list in genre_lists for genre in genre_list]
-genre_counts = pd.Series(genres).value_counts()
-genre_count_str = genre_counts.to_string()
-lines = genre_count_str.split('\n')
-for line in lines[:10]:
-    print(line + ' times')
-print("")
-
+#endregion
+#region Functions
 def filter_by_genre(genre):
     filtered_df = df[df['Genres'].str.contains(genre)]
     filtered_df.loc[:, 'Score'] = pd.to_numeric(filtered_df['Score'], errors='coerce')
@@ -54,16 +36,28 @@ def get_genres(csv_path):
     
     return genre_dict
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from matplotlib.ticker import FixedLocator
+def select_genre():    
+    for key, value in genre_dict.items():
+       print(f"{key}: {value}")
+    
+    while True:
+        selection = input("Enter the genre number you want to select (1-43): ")
+
+        if not selection.isdigit() or int(selection) < 1 or int(selection) > 43:
+            print("Incorrect genre. Please select a number between 1 and 43.")
+        else:
+            break
+
+    genre = genre_dict.get(int(selection))
+    return genre
+
 
 def plot(df,dfxlabel,dfylabel,dftitle):
     
     x = df[dfxlabel].values
     y = df[dfylabel].values
     
-    plt.figure(figsize=(500, 70)) # 1300*700
+    plt.figure(figsize=(50, 7))
     sns.set_style('darkgrid')
     plt.xlabel(dfxlabel)
     plt.ylabel(dfylabel)
@@ -82,26 +76,41 @@ def plot(df,dfxlabel,dfylabel,dftitle):
 
     # Show the values for each bar
     for index, value in enumerate(y):
-        plt.text(index, value - 0.05, str(value), color='black', ha="center")
+        plt.text(index, value - 0.05, str(round(value, 2)), color='black', ha="center")
+
 
     # Set the x-axis labels on the plot object
     lenght = 22
     barplot.set_xticklabels([label.get_text()[:lenght]+'...' if len(label.get_text())>lenght else label.get_text() for label in x_labels])
+#endregion
 
+print("\n╔══════════════════════════════════════════╗")
+print("║  Welcome to the anime analysis program!  ║")
+print("╚══════════════════════════════════════════╝")
+
+#region Top 10 animes by score
+print("\nTop 10 animes by average score:\n")
+filtered_df = df[df['Score'] != 'Unknown'] #Remove Unknown Values
+filtered_df.loc[:, 'Score'] = pd.to_numeric(filtered_df['Score'], errors='coerce')
+filtered_df = filtered_df.dropna(subset=['Score'])
+sorted_df = filtered_df.sort_values('Score', ascending=False) #Sort in descending order
+sorted_df['Score'] = sorted_df['Score'].astype(float)
+top_10_animes = sorted_df.head(10)
+print(top_10_animes[['Name', 'Score']].to_string(index=False))
+#endregion
+#region Top 10 genres
+print("\nThe 10 genres that appear the most among them are:")
+genre_lists = top_10_animes['Genres'].str.split(',')
+genres = [genre.strip() for genre_list in genre_lists for genre in genre_list]
+genre_counts = pd.Series(genres).value_counts()
+genre_count_str = genre_counts.to_string()
+lines = genre_count_str.split('\n')
+for line in lines[:10]:
+    print(line + ' times')
+print("")
+#endregion
+#region Plot all genre available scores
 genre_dict = get_genres(csv_path)
-def select_genre():    
-    for key, value in genre_dict.items():
-       print(f"{key}: {value}")
-    genre = genre_dict.get(int(input("Enter the genre number you want to select (1-43): ")))   
-    return genre
-
-selected_genre = select_genre() 
-top10chosen = filter_by_genre(selected_genre)
-
-print(f"\nYou selected {selected_genre}.\nShowing top 10 animes with the {selected_genre} genre: \n")
-print(top10chosen[['Name', 'Score']].to_string(index=False))
-plot(top10chosen,'Name','Score',f'Top 10 {selected_genre} Animes by Score')
-plt.show()
 result = {}
 for genre in genre_dict.values():
     if genre != "Genres":
@@ -112,14 +121,21 @@ for genre in genre_dict.values():
 genres_average_df = pd.DataFrame(result.items(), columns=['Genre', 'Average Score'])
 genres_average_df = genres_average_df.sort_values(by='Average Score', ascending=False)
 top_genres_average_df = genres_average_df.head(10)
-#plot(genres_average_df,'Genre','Average Score','Top Genres by Score')
+plot(genres_average_df,'Genre','Average Score','Top Genres by Score')
+plt.xticks(rotation=45)
 
-'''
-plt.figure(figsize=(13, 6))
-sns.set_style('darkgrid')
-sns.barplot(x=top_genres_average_df['Genre'], y=top_genres_average_df['Average Score'],color='tan')
-plt.xlabel("Genres")
-plt.ylabel("")
-plt.ylim(6.5, 7.2)
 plt.show()
-'''
+#endregion
+#region Plot by Genre Selection
+
+
+selected_genre = ""
+selected_genre = select_genre() 
+top10chosen = filter_by_genre(selected_genre)
+
+print(f"\nYou selected {selected_genre}.\nShowing top 10 animes with the {selected_genre} genre: \n")
+print(top10chosen[['Name', 'Score']].to_string(index=False))
+plot(top10chosen,'Name','Score',f'Top 10 {selected_genre} Animes by Score')
+
+plt.show()
+#endregion
